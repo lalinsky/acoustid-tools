487845e67e8037223d15a34ebd2d406ffa8485c0
Lukáš Lalinský <lalinsky@gmail.com>
2011-05-11 13:23:00 +0200

    Remove local chromaprint.py

--- acoustid-query.py
+++ acoustid-query.py
@@ -4,13 +4,13 @@ import re
 import sys
 import time
 import os
-from chromaprint import Fingerprinter
+from chromaprint import Fingerprinter, decode_fingerprint
 from audiodecoder import AudioDecoder
 
 #print libchromaprint.chromaprint_get_version()
 print "=> Decoding audio data"
 decoder = AudioDecoder(sys.argv[1])
-length, sample_rate, num_channels, data = decoder.decode()
+length, sample_rate, num_channels, data = decoder.decode(45)
 print "=> Length:", length
 print "=> Sample rate:", sample_rate
 print "=> Number of channels:", num_channels
@@ -20,27 +20,32 @@ fpcal = Fingerprinter()
 fpcal.start(sample_rate, num_channels)
 fpcal.feed(data)
 fingerprint = fpcal.finish()
+print fingerprint
+print decode_fingerprint(fingerprint)
 
 import urllib, urllib2
-from xml.etree import ElementTree
+import json
 
 data = {}
 data['client'] = '8XaBELgH'
+data['format'] = 'json'
 data['meta'] = '2'
-data['length'] = str(length)
+data['duration'] = str(length)
 data['fingerprint'] = fingerprint
 print "=> Looking up fingerprint"
 a = time.time()
-resp = urllib2.urlopen('http://api2.acoustid.org/lookup', urllib.urlencode(data))
-tree = ElementTree.parse(resp)
+resp = urllib2.urlopen('http://api.acoustid.org/v2/lookup', urllib.urlencode(data))
+tree = json.loads(resp.read())
+import pprint
+pprint.pprint(tree)
 print time.time() - a
-#ElementTree.dump(tree)
-for result in tree.findall('results/result'):
-    print
-    print 'Score:', result.find('score').text
-    print 'ID:', result.find('id').text
-    for track in result.findall('tracks/track'):
-        print 'http://musicbrainz.org/track/%s.html' % track.find('id').text
+#for result in tree.findall('results/result'):
+#    print
+#    print 'Score:', result.find('score').text
+#    print 'ID:', result.find('id').text
+#    for track in result.findall('tracks/track'):
+#        print 'http://musicbrainz.org/track/%s.html' % track.find('id').text
+#        for track in track.findall('recordings'):
 
 
 
